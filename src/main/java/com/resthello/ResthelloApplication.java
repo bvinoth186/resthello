9448a820a771659cc24bf347a4f32062eb70bdee
Vinoth <vinoth186@gmail.com>
2018-05-25 15:11:02 +0530

    Init

---
+++ src/main/java/com/resthello/ResthelloApplication.java
@@ -0,0 +1,12 @@
+package com.resthello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResthelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResthelloApplication.class, args);
+	}
+}
